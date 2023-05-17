17eab525f4c6e633541dc88cf9b12681463fafe1
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-05-17 15:48:56 +0500

    Add UzumApplication

---
+++ src/test/java/uz/java/uzum/UzumApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.java.uzum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

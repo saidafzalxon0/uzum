d221c9dc4ab41e290305426ea002c9f3239e01a7
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-05-18 19:17:32 +0500

    Order

---
+++ src/main/java/uz/java/uzum/entity/Cart.java
@@ -0,0 +1,42 @@
+package uz.java.uzum.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Cart {
+    @Id
+    @GeneratedValue(generator = "id",strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "total_price")
+    private Integer totalPrice;
+    @ManyToMany
+    @JoinTable(
+            name = "cart_product",
+            joinColumns = @JoinColumn(name = "cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @CreationTimestamp
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+
+
+}
---
+++ src/main/java/uz/java/uzum/entity/OrderDetail.java
@@ -0,0 +1,27 @@
+package uz.java.uzum.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetail {
+    @Id
+    @GeneratedValue(generator = "id",strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    private User user;
+    private Double total;
+//    @OneToOne
+//    private PaymentDetails paymentDetails;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+}
